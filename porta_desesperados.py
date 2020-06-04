6bde8267b14330849cb2824020556b77034c2d5c
Pena <millor@gmail.com>
2020-06-04 06:41:23 -0300

    Update porta_desesperados.py
    Clean up

--- porta_desesperados.py
+++ porta_desesperados.py
@@ -7,6 +7,7 @@ Created on Wed May 27 19:32:01 2020
 Chance do Serginho Malandro saber a porta do prêmio, dado que
 ele não estragou o quadro (não revelou o prêmio) "n" vezes seguidas
 """
+import timeit
 
 
 def ProbKnowGivenDontScrewUp(n):
@@ -27,30 +28,19 @@ def SameFunctionButNotRecursive(n):
     return P1
 
 
-n = 50
-print(ProbKnowGivenDontScrewUp(n))
-
-
-# Run time measure ----------------------------------
-import timeit
-
-code = """
-import timeit
-
+def CompareRunTimeOnBothFunctions(runs=10000):
+    # Run time measure ----------------------------------
+    code = """
 def ProbKnowGivenDontScrewUp(n):
     if n == 0:
         return 1/2
     else:
         return 1/(1/3 + 2/(3*ProbKnowGivenDontScrewUp(n-1)))
-
 n = 100
 ProbKnowGivenDontScrewUp(n)
 """
-# print("Recursive Function: {}" .format(timeit.timeit(code, number=100000)))
-
-code = """
-import timeit
-
+    print("Recursive Function: {}" .format(timeit.timeit(code, number=runs)))
+    code = """
 def SameFunctionButNotRecursive(n):
     P0 = 0
     for i in range(n+1):
@@ -60,8 +50,11 @@ def SameFunctionButNotRecursive(n):
             P1 = 1/(1/3 + 2/(3*P0))
         P0 = P1
     return P1
-
 n = 100
 SameFunctionButNotRecursive(n)
 """
-# print("Iterative Function: {}" .format(timeit.timeit(code, number=100000)))
+    print("Iterative Function: {}" .format(timeit.timeit(code, number=runs)))
+
+if __name__ == "__main__":
+    for n in range(10):
+        print(ProbKnowGivenDontScrewUp(n))
